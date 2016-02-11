dab19070faae972f57e25336c82e9a55a1151e2d
Anthony Maki <4cm4k1@gmail.com>
2016-02-11 14:47:02 -0600

    Initial Commit

---
+++ public/scripts/clientapp.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('#post-data').on('click', clickPostData);
+    $('#get-data').on('click', clickGetData);
+});
+
+function clickPostData() {
+    event.preventDefault();
+    var values = {};
+    $.each($('#post-form').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+
+    $('#post-form').find('input[type=text]').val('');
+
+    console.log(values);
+
+    $.ajax({
+        type: 'POST',
+        url: '/data',
+        data: values,
+        beforeSend: function() {
+            console.log('before!');
+        },
+        success: function(data) {
+            console.log('From Server: ', data);
+        }
+    });
+}
+
+function clickGetData() {
+    event.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/data',
+        success: function(data) {
+            console.log(data);
+        }
+    });
+}
